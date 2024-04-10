a9c9f6bfd89a0927bff512c7ffcda7d2bbf43d7d
慕勋 <muxunpro@gmail.com>
2024-04-10 23:16:37 +0800

    fix: fix LeetCode1702

--- algorithm-leetcode-jvm/src/main/java/org/muxun/algorithm/leetcode/java/greedy/LeetCode1702.java
+++ algorithm-leetcode-jvm/src/main/java/org/muxun/algorithm/leetcode/java/greedy/LeetCode1702.java
@@ -21,25 +21,27 @@ public class LeetCode1702 {
 
     /**
      * 思路: 贪心算法
+     * 根据灵神的思路：
+     * 1. 结果不会包含两个连续的00，因为和操作1互斥，可以变成10从而得到更大的答案
+     * 2. 结果中至多含有一个0，因为和操作2互斥，我们可以通过移位将右边的0移动到左边的0的位置，然后通过操作1变成更大的数
+     * 所以解决问题的思路就在于，至多会出现一个0，所以我们可以将右边的0移到左边0的位置，然后进行操作1，把数变得更大
      * @param binary 二进制字符串
      * @return 调整后的最大二进制字符串
      */
     public String maximumBinaryString(String binary) {
+        int index = binary.indexOf('0');
+        // 证明全是1
+        if (index < 0) {
+            return binary;
+        }
         int n = binary.length();
         char[] arr = binary.toCharArray();
-        int index = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == '0') {
-                while (index <= i || (index < n && arr[index] == '1')) {
-                    index++;
-                }
-                if (index < n) {
-                    arr[index] = '1';
-                    arr[i] = '1';
-                    arr[i + 1] = '0';
-                }
-            }
+        int count = 0;
+        index++;
+        while (index < n) {
+            count += arr[index] - '0';
+            index++;
         }
-        return new String(arr);
+        return "1".repeat(n - count - 1) + "0" + "1".repeat(count);
     }
 }
